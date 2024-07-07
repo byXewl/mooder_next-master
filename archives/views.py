d03b9aa10fea730a049b5bbc030e8fb16d0a2f9d
Xe <3312902243@qq.com>
2024-07-07 19:27:39 +0800

    处理一个反向代理后可以会判断403问题

--- archives/views.py
+++ archives/views.py
@@ -294,13 +294,13 @@ class JavascriptView(LoginRequiredMixin, TemplateView):
 
     def dispatch(self, request, *args, **kwargs):
         referer = request.META.get('HTTP_REFERER', '')
-        try:
-            u1 = urlparse(referer)
-            u2 = urlparse(request.build_absolute_uri())
+        # try:
+        #     u1 = urlparse(referer)
+        #     u2 = urlparse(request.build_absolute_uri())
 
-            if u1.scheme not in ('http', 'https') or u1.netloc != u2.netloc:
-                return self.handle_no_permission()
-        except Exception:
-            return self.handle_no_permission()
+        #     if u1.scheme not in ('http', 'https') or u1.netloc != u2.netloc:
+        #         return self.handle_no_permission()
+        # except Exception:
+        #     return self.handle_no_permission()
 
         return super(JavascriptView, self).dispatch(request, *args, **kwargs)
